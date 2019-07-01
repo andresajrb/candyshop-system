a57c998c23219451a6a817d8569ccc34f9632dc1
Andres <andresajrb@gmail.com>
2019-07-01 10:41:23 -0400

    Se comenzo la funcion update de la clase product y se establecieron los tipos de update que se crearan

--- data/product.js
+++ data/product.js
@@ -34,6 +34,47 @@ let product = {
 
         });
 
+    },
+
+    // Crear una funcion de update que te permita tener varios tipos de update dependiendo de lo que se necesite
+    // tipo 1: name, description y brand
+    // tipo 2: cost y price
+    // tipo 3: isactive
+    // tipo 4: providerid
+
+    update: function(id) {
+
+        return new Promise((resolve, reject) => {
+
+            let query;
+
+            if (id) {
+                query = 'SELECT * FROM `inventario-ventas`.PRODUCTS WHERE PRODUCTID = ' + id.trim();
+            } else {
+                query = 'SELECT * FROM `inventario-ventas`.PRODUCTS ';
+            }
+
+            connection.query(query,
+
+                (err, resultset, fields) => {
+                    if (err) {
+                        reject('Query error:' + err.stack);
+
+                    }
+                    // Resultado en formato JSON
+                    let result = JSON.parse(JSON.stringify(resultset));
+
+                    // Si no se encuentra el usuario solicitado
+                    if (Object.keys(result).length === 0) {
+                        resolve(null);
+                    }
+                    resolve(result);
+                });
+
+
+
+        });
+
     }
 }
 
